54ee53edd9ab72ff92a14df9d7de6b3f0a099ff1
Jessica Filben <jessica.filben@openwaterfoundation.org>
2019-12-27 18:08:11 -0700

    removed unnecessary comments

--- angular-ng-chart/src/app/my-timeseries-large/my-timeseries-large.component.ts
+++ angular-ng-chart/src/app/my-timeseries-large/my-timeseries-large.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from "jquery";
-// import { HttpClient } from '@angular/common/http';
+
 import { Papa } from 'ngx-papaparse';
 import waitForPapaParse from 'wait-for-papa-parse';
 import 'chartjs-plugin-zoom';
 // import hammer from 'angular-hammer';
-// import { timer } from 'rxjs';
-// import { getLocaleDateTimeFormat } from '@angular/common';
 
 @Component({
   selector: 'app-my-timeseries-large',
@@ -14,8 +12,6 @@ import 'chartjs-plugin-zoom';
   styleUrls: ['./my-timeseries-large.component.css']
 })
 export class MyTimeseriesLargeComponent implements OnInit {
-  // csvContent: string;
-  // parsedCsv: string[][];
 
   constructor(private papa: Papa) { }
 
@@ -50,10 +46,7 @@ export class MyTimeseriesLargeComponent implements OnInit {
       }
       min_time = labelsArray[1];
       max_time = labelsArray[labelsArray.length -1];
-      // console.log(min_time);
-      // console.log(max_time);      
-      // console.log(line1);
-      // console.log(labelsArray);
+
       _this.chartLabels = labelsArray;
       _this.chartLegend = true;
       
@@ -100,63 +93,23 @@ export class MyTimeseriesLargeComponent implements OnInit {
             }
           },
         },
-        // plugins: {
-          // zoom: {
-            // pan: {
-            //   enabled: true,
-            //   mode: 'x',
-            //   speed: 10,
-            //   threshold: 10,
-            //   // rangeMin: {
-            //   //   x: null,
-            //   //   y: null
-            //   // },
-            //   // ramgeMax: {
-            //   //   x: null,
-            //   //   y: null
-            //   // }
-            //   onPan: function({chart}) { console.log(`I'm panning!!!`); }
-            // },
-            // zoom: {
-            //   speed: 0.1,
-            //   onZoom: function({chart}) { console.log(`I'm zooming!!!`); },
-            //   enabled: true,
-            //   // drag: true,
-            //   mode: 'x'
-            // },
-          // }
-        // },
         responsive: true,
         scales: {
           xAxes: [{
             type: 'time',
             distribution: 'linear',
-            // bounds: 'ticks',
-            // ticks: {
-            //   beginAtZero: false,
-            // },
             time:{
               min: min_time,
               max: max_time,
-              // format: getLocaleDateTimeFormat,
               tooltipFormat: 'MM/DD/YYYY'
-              // unit: 'year',
-              // displayFormats: 'll'
             },
-            // distribution: 'linear',
             display: true,
             scaleLabel: {
               display: true,
               labelString: "Date",
             }
-            // ticks: {
-            //   maxRotation: 0
-            // }
         }],
           yAxes: [{
-            // ticks: {
-            //   beginAtZero: true,
-            // },
             display: true,
             scaleLabel: {
               display: true,
@@ -168,40 +121,5 @@ export class MyTimeseriesLargeComponent implements OnInit {
       _this.isDataAvaliable = true;
     });
   }
-
-  // public chartData = [
-  //   {
-  //     fill: false,
-  //     label: 'Page Views',
-  //     data: [280, 250, 340, 100, 150, 78, 300, 312],
-  //     // }, {
-  //     //   // x: new Date("2017-08-19T00:00:00-06:00"), 
-  //     //   // y: 250
-  //     // }, {
-  //     //   // x: new Date("2017-08-20T00:00:00-06:00"), 
-  //     //   // y: 280
-  //     // }, {
-  //     //   // x: new Date("2017-08-21T00:00:00-06:00"), 
-  //     //   // y: 250
-  //     // }, {
-  //     //   // x: new Date("2017-08-22T00:00:00-06:00"), 
-  //     //   // y: 300
-  //     // }, {
-  //     //   // x: new Date("2017-08-23T00:00:00-06:00"), 
-  //     //   // y: 200
-  //     // }],
-  //     borderColor: '#800080',
-  //     backgroundColor: '##800080',
-  //     lineTension: 0
-  //   },
-  //   {
-  //     fill: false,
-  //     label: 'Purchases',
-  //     data: [40, 100, 23, 40, 50, 70, 30, 33],
-  //     borderColor: '#5c9da3',
-  //     backgroundColor: '#5c9da3',
-  //     lineTension: 0
-  //   }
-  // ];
 
 }
